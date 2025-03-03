d403e61653acff8b4e0a970ea686aa108451fa58
MaksudMalik <maksudmalik360@gmail.com>
2025-03-04 01:13:48 +0600

    minor fixes

--- app/K_Means_Clustering.py
+++ app/K_Means_Clustering.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 class Kmeans:
     def __init__(self):
@@ -25,34 +22,8 @@ class Kmeans:
     def compute_new_centroids(self,X,assigned_centroids,k):
         centroids = np.array([X[assigned_centroids == i].mean(axis=0) for i in range(k)])
         return centroids
-    
-    def plot_graph(self,X,centroids,assigned_centroids):
-        dimension=X.shape[1]
-        if dimension<4:
-            fig=plt.figure(figsize=(10,6))
-            if dimension==3:
-                ax=fig.add_subplot(111, projection='3d')
-                ax.scatter(X[:,0], X[:,1], X[:,2], c=assigned_centroids, edgecolors="k")
-                ax.scatter(centroids[:,0], centroids[:,1], centroids[:,2], c='red', marker='X', s=200, label="Final Centroids")
 
-            elif dimension==2:
-                ax=fig.add_subplot(111)
-                ax.scatter(X[:,0], X[:,1], c=assigned_centroids, edgecolors="k")
-                ax.scatter(centroids[:,0], centroids[:,1], c='red', marker='X', s=200, label="Final Centroids")
-
-            else:
-                ax=fig.add_subplot(111)
-                ax.scatter(X, np.zeros_like(X), c=assigned_centroids, edgecolors="k")
-                ax.scatter(centroids, np.zeros_like(centroids), c='red', marker='X', s=200, label="Final Centroids")
-            
-            ax.set_title("Scatter Plot of Clusters")
-            ax.legend()
-            plt.show()
-        else:
-            print("Cannot plot multidimensional data (dimension >= 4)")
-        return
-
-    def fit_kmeans(self,X,k,max_iters=15,plot=False):
+    def fit_kmeans(self,X,k,max_iters=15):
         initial_centroids=self.initialize_centroid(X,k)
         centroids=initial_centroids
         for i in range (max_iters):
@@ -62,8 +33,6 @@ class Kmeans:
             if np.all(centroids==new_centroids):
                 break
             centroids=new_centroids
-        if plot:
-            self.plot_graph(X,centroids,assigned_centroids)
         self.centroids=centroids
         self.assigned_centroids=assigned_centroids
         return
