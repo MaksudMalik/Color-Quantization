5d1eeddac5f9fd6079a5b55705560ddcc8ce64e1
MaksudMalik <maksudmalik360@gmail.com>
2025-03-03 22:09:49 +0600

    UI Fixes

---
+++ static/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function updateValue(id) {
+        document.getElementById(id + "_value").textContent = document.getElementById(id).value;
+    }
+    document.getElementById("num_colors").addEventListener("input", function () {
+        updateValue("num_colors");
+    });
+    document.getElementById("iterations").addEventListener("input", function () {
+        updateValue("iterations");
+    });
+
+    const uploadBox = document.getElementById("upload_box");
+    const fileInput = document.getElementById("file_input");
+
+    if (uploadBox && fileInput) {
+        uploadBox.addEventListener("click", function () {
+            fileInput.click();
+        });
+
+        fileInput.addEventListener("change", function () {
+            fileInput.form.submit();
+        });
+    }
+});
