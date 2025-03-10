dec3869ce426edd8bf6f0cd154f91e6647832e85
MaksudMalik <maksudmalik360@gmail.com>
2025-03-11 01:10:09 +0600

    UI Fixes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, Form, Request, Response
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from app.process import process_image
 import numpy as np
@@ -11,42 +11,29 @@ import os
 app = FastAPI()
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "..", "templates"))
 app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "..", "static")), name="static")
+
 ALLOWED_MIME_TYPES = {"image/png", "image/jpeg", "image/jpg"}
 
 @app.get("/")
 async def homepage(request: Request):
-    original_img = request.cookies.get("original_img")
-    processed_img = request.cookies.get("processed_img")
-    
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "original_img_base64": original_img,
-        "processed_img_base64": processed_img,
-    })
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/upload")
 async def upload_image(request: Request, file: UploadFile = File(...)):
     if file.content_type.lower() not in ALLOWED_MIME_TYPES:
-        return templates.TemplateResponse("index.html", {"request": request, "error_message": "UPLOAD A VALID PHOTO"})
+        return JSONResponse(content={"error_message": "UPLOAD A VALID PHOTO"}, status_code=400)
 
     contents = await file.read()
     img_buffer = np.frombuffer(contents, np.uint8)
     image = cv2.imdecode(img_buffer, cv2.IMREAD_COLOR)
+    _, image = cv2.imencode('.png', image)
+    image = base64.b64encode(image).decode('utf-8')
 
-    _, buffer = cv2.imencode('.png', image)
-    encoded_img = base64.b64encode(buffer).decode('utf-8')
-
-    response = RedirectResponse(url="/", status_code=303)
-    response.set_cookie(key="original_img", value=encoded_img, httponly=True, max_age=3600)
-    return response
+    return JSONResponse(content={"original_img_base64": image})
 
 @app.post("/quantize")
-async def quantize_image(request: Request, num_colors: int = Form(...), iterations: int = Form(...)):
-    original_img = request.cookies.get("original_img")
-    if not original_img:
-        return RedirectResponse(url="/", status_code=303)
-
-    original_data = base64.b64decode(original_img)
+async def quantize_image(request: Request, num_colors: int = Form(...), iterations: int = Form(...), img_base64: str = Form(...)):
+    original_data = base64.b64decode(img_base64)
     img_buffer = np.frombuffer(original_data, np.uint8)
     image = cv2.imdecode(img_buffer, cv2.IMREAD_COLOR)
 
@@ -55,6 +42,6 @@ async def quantize_image(request: Request, num_colors: int = Form(...), iteratio
     _, proc_buffer = cv2.imencode('.png', quantized_img)
     processed_img_base64 = base64.b64encode(proc_buffer).decode('utf-8')
 
-    response = RedirectResponse(url="/", status_code=303)
-    response.set_cookie(key="processed_img", value=processed_img_base64, httponly=True, max_age=3600)
-    return response
+    return JSONResponse(content={
+        "processed_img_base64": processed_img_base64
+    })
--- static/script.js
+++ static/script.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    const uploadBox = document.getElementById("upload_box");
+    const fileInput = document.getElementById("file_input");
+    const error_text = document.getElementById("error_text");
+    const before_upload_box = document.getElementById('empty_upload_box');
+    const before_processed_box = document.getElementById('empty_processed_box');
+    const uploadedContent = document.querySelectorAll('.original_image');
+    const processedContent = document.querySelectorAll('.processed_image');
+    const submit_button =  document.getElementById("submit_button")
+    let original_file = null;
+
     function updateValue(id) {
         document.getElementById(id + "_value").textContent = document.getElementById(id).value;
     }
@@ -9,17 +20,69 @@ document.addEventListener("DOMContentLoaded", function () {
         updateValue("iterations");
     });
 
-    const uploadBox = document.getElementById("upload_box");
-    const fileInput = document.getElementById("file_input");
 
-    if (uploadBox && fileInput) {
-        uploadBox.addEventListener("click", function () {
-            fileInput.click();
-        });
+    uploadBox.addEventListener("click", function () {
+        fileInput.click();
+    });
 
-        fileInput.addEventListener("change", function () {
-            fileInput.form.submit();
+
+    fileInput.addEventListener("change", async function () {
+        const formData = new FormData();
+        formData.append("file", fileInput.files[0]);
+        const response = await fetch("/upload", {
+            method: "POST",
+            body: formData
         });
-    }
+        if (response.ok) {
+            const data = await response.json();
+            document.querySelector('img.original_image').src = `data:image/png;base64,${data.original_img_base64}`;
+            original_file = data.original_img_base64;
+            before_upload_box.style.display = 'none';
+            before_processed_box.style.display = 'none';
+            error_text.style.display="none";
+            uploadedContent.forEach(function(element) {
+                element.style.display = "flex";
+            });
+            submit_button.disabled = false;
+        }
+        else{
+            uploadedContent.forEach(function(element) {
+                element.style.display = "none";
+            });
+            processedContent.forEach(function(element) {
+                element.style.display = "none";
+            });
+            before_processed_box.style.display = 'block';
+            before_upload_box.style.display='none'
+            error_text.style.display="block";
+            submit_button.disabled = true;
+        }
+    });
+
+
+    document.getElementById("submit_button").addEventListener("click", async function () {
+        const numColors = document.getElementById("num_colors").value;
+        const iterations = document.getElementById("iterations").value;
+        const formData = new FormData();
+        formData.append("num_colors", numColors);
+        formData.append("iterations", iterations);
+        formData.append("img_base64", original_file);
+        const response = await fetch("/quantize", {
+            method: "POST",
+            body: formData
+        });
+        if (response.ok) {
+            const data = await response.json();
+            document.querySelector('img.processed_image').src = `data:image/png;base64,${data.processed_img_base64}`;
+            uploadedContent[2].style.display = 'none';
+            processedContent.forEach(function(element) {
+                element.style.display = "flex";
+            });
+            
+        } else {
+            const data = await response.json();
+            alert(data.error_message);
+        }
+    });
+
 });
-
